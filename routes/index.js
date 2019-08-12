e06f85cfa43e48698c0032c562bd10960bf1198d
Calvin Shin <calvin.shin@gmail.com>
2019-08-11 22:37:18 -0700

    Added routes and fixed the order so the backend could provide data to the front end

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
 const users = require('./api/users.js');
-const reservations = require("./reservations")
+const grill = require("./grill")
 
 
 router.use('/api', users);
-router.use("/reservations", reservations)
+router.use("/grill", grill)
 
 router.get('/', (req, res) => {
   res.status(200).json({ message: 'Connected!' });
 });
 
-module.exports = routes;
+module.exports = router;
